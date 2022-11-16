89e056097a449b0152f11d05ffb915d81e04cdbb
Sharon Goliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2022-11-16 13:59:56 -0800

    CADC-11992 - some changes to testing strategy - get COLLECTION from the config.yml file.

--- blank2caom2/tests/conftest.py
+++ blank2caom2/tests/conftest.py
@@ -3,7 +3,7 @@
 # ******************  CANADIAN ASTRONOMY DATA CENTRE  *******************
 # *************  CENTRE CANADIEN DE DONNÉES ASTRONOMIQUES  **************
 #
-#  (c) 2020.                            (c) 2020.
+#  (c) 2022.                            (c) 2022.
 #  Government of Canada                 Gouvernement du Canada
 #  National Research Council            Conseil national de recherches
 #  Ottawa, Canada, K1A 0R6              Ottawa, Canada, K1A 0R6
@@ -67,65 +67,4 @@
 # ***********************************************************************
 #
 
-"""
-This module implements the ObsBlueprint mapping, as well as the workflow 
-entry point that executes the workflow.
-"""
-
-from caom2pipe import caom_composable as cc
-from caom2pipe import manage_composable as mc
-
-
-__all__ = [
-    'BlankMapping',
-    'BlankName',
-    'APPLICATION', 
-]
-
-
-APPLICATION = 'blank2caom2'
-
-
-class BlankName(mc.StorageName):
-    """Naming rules:
-    - support mixed-case file name storage, and mixed-case obs id values
-    - support uncompressed files in storage
-    """
-
-    BLANK_NAME_PATTERN = '*'
-
-    def __init__(self, entry=None):
-        self.fname_in_ad = entry
-        super(BlankName, self).__init__(
-            obs_id=entry,
-            product_id=entry,
-            file_name=entry,
-            source_names=[entry],
-        )
-
-    def is_valid(self):
-        return True
-
-
-class BlankMapping(cc.TelescopeMapping):
-    def __init__(self, storage_name, headers):
-        super().__init__(storage_name, headers)
-
-    def accumulate_bp(self, bp, application=None):
-        """Configure the telescope-specific ObsBlueprint at the CAOM model
-        Observation level."""
-        self._logger.debug('Begin accumulate_bp.')
-        super().accumulate_blueprint(bp, APPLICATION)
-        bp.configure_position_axes((1, 2))
-        bp.configure_time_axis(3)
-        bp.configure_energy_axis(4)
-        bp.configure_polarization_axis(5)
-        bp.configure_observable_axis(6)
-        self._logger.debug('Done accumulate_bp.')
-
-    def update(self, observation, file_info, caom_repo_client):
-        """Called to fill multiple CAOM model elements and/or attributes
-        (an n:n relationship between TDM attributes and CAOM attributes).
-        """
-        super().update(observation, file_info, caom_repo_client)
-        return observation
+COLLECTION = 'BLANK'
