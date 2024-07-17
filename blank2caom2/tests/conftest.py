4f7665adc8d0ca2a00cd7fc4118a16ab31ed5e81
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2024-07-16 17:51:10 -0700

    Update conftest.py

--- blank2caom2/tests/conftest.py
+++ blank2caom2/tests/conftest.py
@@ -66,6 +66,7 @@
 # ***********************************************************************
 #
 
+from os.path import dirname, join, realpath
 from caom2pipe.manage_composable import Config, StorageName
 import pytest
 
@@ -86,3 +87,14 @@ def test_config():
     StorageName.scheme = config.scheme
     return config
 
+
+@pytest.fixture()
+def test_data_dir():
+    this_dir = dirname(realpath(__file__))
+    fqn = join(this_dir, 'data')
+    return fqn
+
+             
+@pytest.fixture()
+def change_test_dir(tmp_path, monkeypatch): 
+    monkeypatch.chdir(tmp_path)
