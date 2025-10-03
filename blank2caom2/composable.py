39bf491cdc1e3d127163a64a5e2fb7cb7683d46b
SharonGoliath <Sharon.Goliath@nrc-cnrc.gc.ca>
2025-10-03 13:30:38 -0700

    Update composable.py

--- blank2caom2/composable.py
+++ blank2caom2/composable.py
@@ -80,6 +80,7 @@ import traceback
 
 from caom2pipe.run_composable import run_by_state_runner_meta, run_by_todo_runner_meta
 from blank2caom2 import file2caom2_augmentation
+from blank2caom2.main_app import BlankName
 
 
 META_VISITORS = [file2caom2_augmentation]
@@ -93,7 +94,9 @@ def _run():
     :return 0 if successful, -1 if there's any sort of failure. Return status
         is used by airflow for task instance management and reporting.
     """
-    return run_by_todo_runner_meta(meta_visitors=META_VISITORS, data_visitors=DATA_VISITORS)
+    return run_by_todo_runner_meta(
+        meta_visitors=META_VISITORS, data_visitors=DATA_VISITORS, storage_name_ctor=BlankName
+    )
 
 
 def run():
@@ -111,7 +114,9 @@ def run():
 def _run_incremental():
     """Uses a state file with a timestamp to identify the work to be done.
     """
-    return run_by_state_runner_meta(meta_visitors=META_VISITORS, data_visitors=DATA_VISITORS)
+    return run_by_state_runner_meta(
+        meta_visitors=META_VISITORS, data_visitors=DATA_VISITORS, storage_name_ctor=BlankName
+    )
 
 
 def run_incremental():
